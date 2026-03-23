eb8b372aff11849d23df37da9a71f3ba6776daaf
dmpt1992 <developwebsitedanielamedeira@gmail.com>
2026-03-23 20:51:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// smooth scroll feel (tipo premium)
-const container = document.querySelector(".scroll-container");
 
-container.addEventListener("wheel", (e) => {
-  e.preventDefault();
-  container.scrollBy({
-    top: e.deltaY * 1.2,
-    behavior: "smooth"
-  });
-}, { passive: false });
